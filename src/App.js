8450163af54c32f349625fc62c5148570fc6b412
Arecio Canton <cuba289@gmail.com>
2021-05-15 20:15:51 -0400

    add task

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 // for classes
 // import React from 'react'
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import { useState } from 'react'
+import AddTask from './components/AddTask'
+
+
 function App() {
   const [tasks, setTasks] = useState([
     {
@@ -31,10 +34,17 @@ function App() {
       setTasks(tasks.filter((task) => task.id !== id))
     }
 
+
+    // Toggle Reminder
+    const toggleReminder = (id) => {
+      console.log(id);
+      setTasks(tasks.map((task) => task.id === id ? {  ...task, reminder: !task.reminder}: task))
+    }
   return (
     <div className="container">
      <Header  />
-    {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask}/> ) : ("You don't have any Tasks!" )}
+     <AddTask />
+    {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> ) : ("You don't have any Tasks!" )}
     </div>
   );
 }
